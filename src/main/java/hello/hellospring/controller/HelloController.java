70e00d07f08cbdfc5b1d358364c9de4a9cb32c49
brian <brianhwang29@gmail.com>
2020-12-17 12:03:47 +0900

    1217_basicAPI_JSON

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -24,5 +25,52 @@ public class HelloController {
     public String studyMvc(@RequestParam(value="name", defaultValue="selfStudy") String name, Model model){
         model.addAttribute("name", name);
         return "studyMvc";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; // "hello spring"
+    }
+
+    //API 중요한 부분
+    @GetMapping("hello-api")
+    @ResponseBody // --> spring에서 기본 JSON으로 반환
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        hello.setAge(29);
+        hello.setAddress("강남시");
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+        private int age;
+        private String address;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
     }
 }
